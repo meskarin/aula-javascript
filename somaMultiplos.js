7d29e4f079d47dd0a77a8aa40ddb248a13ade223
meskarin <rafael.ide@outlook.com>
2022-02-04 00:57:51 -0300

    Alteração no console.log

--- somaMultiplos.js
+++ somaMultiplos.js
@@ -1,4 +1,4 @@
-function somarMultiplos(indMax) {
+function somarMultiplos5(indMax) {
 
     let multiplosDe5 = 0;
 
@@ -6,7 +6,20 @@ function somarMultiplos(indMax) {
         if(i % 5 === 0 )
         multiplosDe5 += i;
     }
-    console.log(multiplosDe5);
+    console.log(`A soma dos multiplos de 5 é ${multiplosDe5}`);
 }
 
-somarMultiplos(1000)
+function somarMultiplos7(indMax) {
+
+    let multiplosDe7 = 0;
+
+    for( i = 0; i <= indMax; i++){
+        if(i % 7 === 0 )
+        multiplosDe7 += i;
+    }
+    console.log(`A soma dos multiplos de 7 é ${multiplosDe7}`);
+}
+
+
+somarMultiplos5(1000)
+somarMultiplos7(1000)
